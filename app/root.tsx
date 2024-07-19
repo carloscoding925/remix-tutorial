9e80dafe8b952a306fe3f6fcb70acf99595edddc
Carlos Hernandez <carlosandresh925@gmail.com>
2024-07-19 11:40:36 -0700

    Created routes directory

--- app/root.tsx
+++ app/root.tsx
@@ -5,13 +5,22 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref }
+];
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1" 
+        />
         <Meta />
         <Links />
       </head>
@@ -27,7 +36,11 @@ export default function App() {
                 type="search"
                 name="q"
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div 
+                id="search-spinner" 
+                aria-hidden 
+                hidden={true} 
+              />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
